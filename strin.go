f579fb132cb2dd1a90f9301653909516e4052b2f
Axel <axel.paulander@gmail.com>
2019-01-26 23:49:25 +0100

    empty input fix

--- strin.go
+++ strin.go
@@ -11,7 +11,9 @@ func Strin(mess string) string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print(mess)
 	text, _ := reader.ReadString('\n')
-	if text[len(text)-2] == '\r' {
+	if len(text) < 1 {
+		return ""
+	} else if text[len(text)-2] == '\r' {
 		return text[:len(text)-2]
 	}
 	return text[:len(text)-1]
